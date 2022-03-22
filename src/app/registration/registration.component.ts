6bf830f553170d594eafb5206590a6ccca871b95
Denikaev Emil <edenikaev@mail.ru>
2022-03-22 18:24:36 +0400

    ФИО

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,41 +7,12 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
-
-  form!: FormGroup
-  submitted = false
-  message!: string;
-
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) {
+ constructor() { }
+  form = new FormGroup({
+    soname: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required)
+  })
+  ngOnInit(): void {
+    
   }
-
-  ngOnInit() {
-    this.route.queryParams.subscribe((Params) =>{
-      if (Params['loginAgain']){
-        this.message = 'Пожалуйста, введите данные'
-      } else if (Params['authFailed']) {
-        this.message = 'Сессия истекла. Введите данные'
-      }
-    })
-
-    this.form = new FormGroup({
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.email
-      ]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6)
-      ])
-    })
-  }
-
-  submit() {
-
-  }
-
 }
